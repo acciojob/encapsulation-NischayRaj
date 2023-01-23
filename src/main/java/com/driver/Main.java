7d92e98856dd7a657a0e119d55b144bb895aa7a7
Nischay <nischayraj11@gmail.com>
2023-01-23 13:06:41 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,7 @@ package com.driver;
     public class Main {
         public static void main(String[] args) {
             RWOnly obj = new RWOnly();
-            // obj.name = "John"; // error: name has private access in RWOnly
-            // System.out.println(obj.name); // error: name has private access in RWOnly
+
             obj.setName("John");
             System.out.println(obj.getName());
         }
